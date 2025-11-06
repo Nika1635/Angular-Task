d02c69d0a1b2810390af4e125be692caa54ecfe5
nika robakidze <nika70252@gmail.com>
2025-11-07 01:00:21 +0400

    routerFix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { TodosComponent } from './pages/todos/todos.component';
 import { PromotionsComponent } from './pages/promotions/promotions.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'users' },
+  { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'posts', component: PostsComponent },
   { path: 'todos/:userId', component: TodosComponent },
