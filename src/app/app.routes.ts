0f2a198057a314abdcf6199e3455471ff35919f8
nika robakidze <nika70252@gmail.com>
2025-11-07 00:57:59 +0400

    component-structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './pages/users/users.component';
+import { PostsComponent } from './pages/posts/posts.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { PromotionsComponent } from './pages/promotions/promotions.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'users' },
+  { path: 'users', component: UsersComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'todos/:userId', component: TodosComponent },
+  { path: 'promotions', component: PromotionsComponent },
+  { path: '**', redirectTo: 'users' }
+];
