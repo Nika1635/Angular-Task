f1f039075afa97d81c76820bf37d0e4b06e7da9e
nika robakidze <nika70252@gmail.com>
2025-11-08 22:07:28 +0400

    wheel-file-transfer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { UsersComponent } from './pages/users/users.component';
 import { TodosComponent } from './pages/todos/todos.component';
 import { PromotionsComponent } from './pages/promotions/promotions.component';
+import { UserpostComponent } from './pages/userpost/userpost.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
   { path: 'todos/:userId', component: TodosComponent },
-  { path: 'promotions', component: PromotionsComponent },
+  { path: 'userposts/:userId', component: UserpostComponent},
+  { path: 'promotions', component: PromotionsComponent},
   { path: '**', redirectTo: 'users' }
 ];
